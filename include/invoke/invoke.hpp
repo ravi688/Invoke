b9f82bbbe2ce3c3fe191c00f005dbe87ebebf145
ravi688 <rp0412204@gmail.com>
2025-03-28 13:54:44 +0530

    Removed stray code

--- include/invoke/invoke.hpp
+++ include/invoke/invoke.hpp
@@ -12,14 +12,4 @@ namespace invoke
 {
 	INVOKE_API std::optional<std::vector<std::string>> GetExecutablePaths(std::string_view executable);
 	INVOKE_API int Exec(const std::vector<std::string>& args, std::string_view workDir = "");
-
-	struct SwitchToSudo
-	{
-		// Switch to sudo if the process was given sudo privileges when launched
-		SwitchToSudo();
-		// Switch back to non-sudo
-		~SwitchToSudo();
-	};
 }
-
-#define INVOKE_SWITCH_TO_SUDO CAN_BE_UNUSED_VARIABLE invoke::SwitchToSudo ____##__LINE__;
